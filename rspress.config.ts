c71230ca4c667d4b0225f1896db10cfb7ba4f9a8
Single <1197826934@qq.com>
2024-12-12 16:02:45 +0800

    fix(deployment): 资源找不到

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
+  base: '/achobeta-portal/',
   title: 'AchoBeta',
   icon: '/achobeta.jpg',
   logoText: 'AchoBeta',
