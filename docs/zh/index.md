026ad82e54e80b580dd20f72d30999442bec7fec
Single <1197826934@qq.com>
2024-12-12 15:17:47 +0800

    feat: init project

---
+++ docs/zh/index.md
@@ -0,0 +1,28 @@
+---
+pageType: home
+
+hero:
+  name: AchoBeta
+  text: 用冰冷的技术，赋予生活的无限温柔 
+  tagline: <img src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=800&size=24&duration=2000&pause=4000&color=3C3C3CA8&center=true&vCenter=true&width=435&lines=%E8%81%9A%E6%98%AF%E4%B8%80%E5%9B%A2%E7%81%AB%EF%BC%8C%E6%95%A3%E6%98%AF%E6%BB%A1%E5%A4%A9%E6%98%9F!" alt="Typing SVG" />
+  actions:
+    - theme: brand
+      text: 了解我们
+      link: /guide/
+    - theme: alt
+      text: GitHub
+      link: https://github.com/web-infra-dev/rspress
+  image:
+    src: /achobeta.svg
+    alt: Logo
+features:
+  - title: 竞赛荣誉与学分加成
+    details: 参与挑战杯、计算机设计大赛等顶尖赛事，全面提升个人实力。同时获得课外学分 + 综测分 + 荣誉证书，进一步丰富个人简历.
+    icon: 👑
+  - title: 技术交流与经验分享
+    details: 不定期团建活动，认识志同道合的好友，深入技术交流，考研+就业等等经验分享。同时结交优秀热情的创业合作伙伴，获得更多创业机会.
+    icon: 📖
+  - title: 名企内推与资源共享
+    details: 可与就业名企的师兄师姐们探讨技术知识也可获取内推资格，不定期组织技术分享会提前接触企业工作流程，各种丰富资源等你来拿~
+    icon: 💞
+---
---
+++ rspress.config.ts
@@ -0,0 +1,49 @@
+import * as path from 'node:path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'AchoBeta',
+  icon: '/achobeta.jpg',
+  logoText: 'AchoBeta',
+  lang: 'zh',
+  logo: {
+    light: '/achobeta.svg',
+    dark: '/achobeta.svg',
+  },
+  locales: [
+    {
+      lang: 'en',
+      label: 'English',
+      title: 'AchoBeta',
+      description: 'A software development innovation laboratory full of passion.',
+    },
+    {
+      lang: 'zh',
+      label: '简体中文',
+      title: 'AchoBeta',
+      description: '一个充满热情的软件开发创新实验室',
+    },
+  ],
+  themeConfig: {
+    locales: [
+      {
+        label: 'English',
+        lang: 'en',
+        outlineTitle: 'ON THIS Page',
+      },
+      {
+        label: '简体中文',
+        lang: 'zh',
+        outlineTitle: '大纲',
+      },
+    ],
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/AchoBeta',
+      },
+    ],
+  },
+});
